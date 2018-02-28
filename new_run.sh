563ad1decaabb31de3013de81b9d6a605456cdcb
Anryan <2665572581@qq.com>
2018-02-28 14:21:41 +0800

    Update new_run.sh

--- new_run.sh
+++ new_run.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 echo "start up mining"
-bullockchain-cli  -conf=/home/jack/bitcoin.conf generatethread true
+bullockchain-cli  -conf=/home/jack/bitcoin.conf generatethread true 
 echo "mining now"
-function transafer(){
+#var=QQQQQ$RANDOM
+#function transafer(){
  
-echo "start to transfer"
-bullockchain-cli  -conf=/home/jack/bitcoin.conf sendtoaddress 1AGoxBiXYTsRtyK6p9swXRaWqdiJ9uUd7o GAS 1
+  #echo "start to transfer"
+ # bullockchain-cli  -conf=/home/jack/bitcoin.conf sendtoaddress 1AGoxBiXYTsRtyK6p9swXRaWqdiJ9uUd7o GAS 1
 
-}
- function createtoken(){
- result=` bullockchain-cli  -conf=/home/jack/bitcoin.conf   createtoken 1EPs2V3G3xC3rXZm4bmVAQo8wfeReKwLcY  QQQQQ$RANDOM 10000 10000 true`
- }
- while true 
- do
- createtoken()
- result2=` bullockchain-cli  -conf=/home/jack/bitcoin.conf gettransaction $result |grep confirmations|awk '{print $2}'|sed 's/\,//g'4`
-if [ $result2 -gt 10 ]
- then
-   transfer()
- fi
- sleep 10
- done
+#}
+#function createtoken(){
+ #result=` bullockchain-cli  -conf=/home/jack/bitcoin.conf   createtoken 1EPs2V3G3xC3rXZm4bmVAQo8wfeReKwLcY  $var 10000 10000 true`
+
+#}
+#result=` bullockchain-cli  -conf=/home/jack/bitcoin.conf   createtoken 1EPs2V3G3xC3rXZm4bmVAQo8wfeReKwLcY  $var 10000 10000 true`
+while true 
+do
+#发布随机名称的币种英文字符加随机数，全局变量多出调用
+var=QQQQQ$RANDOM
+#发行
+result=` bullockchain-cli  -conf=/home/jack/bitcoin.conf   createtoken 1EPs2V3G3xC3rXZm4bmVAQo8wfeReKwLcY  $var 10000 10000 true`
+echo "this is the txid of createtoken $result"
+#转账
+result2=` bullockchain-cli  -conf=/home/jack/bitcoin.conf gettransaction $result |grep confirmations|awk '{print $2}'|sed 's/\,//g'`
+echo "the confirmation of currency is $result2"
+sleep 10
+#if [[ $result2 -gt 10 ]]
+#then
+  result3=`bullockchain-cli  -conf=/home/jack/bitcoin.conf sendtoaddress 1AGoxBiXYTsRtyK6p9swXRaWqdiJ9uUd7o AAA 0.1`
+ echo "this is the txid of the transaction $result3"
+#fi
+sleep 15
+done
